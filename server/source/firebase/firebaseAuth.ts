467ba2e93445e49ea7549d2b52cf6e65ca030a0d
LeanderAK <82841550+LeanderAK@users.noreply.github.com>
2023-01-11 23:59:07 +0100

    added api for firebase routing

---
+++ server/source/firebase/firebaseAuth.ts
@@ -0,0 +1,90 @@
+import { resolve } from "dns";
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
+
+import { firebaseApp } from "./firebaseConfig";
+
+const auth = getAuth();
+
+function getCurrentUser() {
+  return new Promise((resolve, reject) =>{
+    const unsubscribe = auth.onAuthStateChanged(function(user) {
+        if(user) {
+          resolve(user)
+        } else {
+          resolve(null)
+        }
+        unsubscribe()
+      })
+  })
+  
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+    //Redux - Statemanager for react
+    console.log("signed in");
+  } else {
+    // User is signed out
+    // ...
+    console.log("not signed in");
+  }
+});
+
+async function registerUser(email: string, password: string) {
+  const auth = getAuth(firebaseApp);
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Registered
+      return "";
+    })
+    .catch((error) => {
+      // Firebase: Error (auth/invalid-email).
+      // Firebase: Password should be at least 6 characters (auth/weak-password).
+      // Firebase: Error (auth/email-already-in-use).
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      return errorCode;
+    });
+}
+
+async function loginUser(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      return "";
+    })
+    .catch((error) => {
+      console.log(error.message);
+      // Firebase: Error (auth/wrong-password).
+      // Firebase: Error (auth/invalid-email).
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      return errorCode;
+    });
+}
+
+async function logoutUser() {
+  const auth = getAuth(firebaseApp);
+  signOut(auth)
+    .then(() => {
+      //Success
+    })
+    .catch((error) => {
+      //Failed with error
+    });
+}
+
+export { registerUser, loginUser, logoutUser, getCurrentUser };
