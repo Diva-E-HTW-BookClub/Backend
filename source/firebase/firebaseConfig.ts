b266e4bf5588feeffdbde8a4c60cc39994a45880
LeanderAK <82841550+LeanderAK@users.noreply.github.com>
2023-03-05 21:45:43 +0900

    removed firebase config data

--- source/firebase/firebaseConfig.ts
+++ source/firebase/firebaseConfig.ts
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyDarZyJqdfwiqwwsHMnzHqm5IPwHgeaUQQ",
-  authDomain: "diva-e-htw-bookclub.firebaseapp.com",
-  projectId: "diva-e-htw-bookclub",
-  storageBucket: "diva-e-htw-bookclub.appspot.com",
-  messagingSenderId: "322441309453",
-  appId: "1:322441309453:web:b93823d0d867546070d444",
-  measurementId: "G-XMMQYQLYFT"
+  // Add Firebase Config data here
 };
 
 // Initialize Firebase
